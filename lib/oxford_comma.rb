1aedc27420b4605dc97367cfc639bf82b1b79487
Andres <andresjml@gmail.com>
2021-06-30 17:39:33 -0400

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+    if array.size==1
+        array.join
+
+    elsif array.size==2
+        first_value=array.shift
+        second_value=" and "+ array.pop
+        string=first_value+second_value       
+        string
+            
+    else
+        last_value=", and "+ array.pop
+        string=array.join(", ")
+        string << last_value
+        string
+        
+    end    
+    
 
 end
